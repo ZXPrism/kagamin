0ec3b821d30ac21ee6ec5647216645e0ed3e24e7
ZXPrism <2897544036@qq.com>
2025-08-16 15:19:02 +0800

    feat: bind group create info + bind group entry

--- src/bind_group.rs
+++ src/bind_group.rs
@@ -1,23 +1,60 @@
-use std::sync::Arc;
+use std::{default, sync::Arc};
+
+use log::error;
 
 pub struct BindGroup {
-    pub buffer: Arc<Vec<f64>>,
+    buffers: Vec<Arc<Vec<f64>>>,
 }
 
 pub struct BindGroupCreateInfo {
-    entries: ;
+    pub entries: Vec<BindGroupEntry>,
 }
 
-pub struct BindGroupEntry{
-    binding_point : ;
+pub struct BindGroupEntry {
+    pub binding_point: usize,
+    pub buffer: Arc<Vec<f64>>,
 }
 
 impl BindGroup {
     pub fn new(create_info: &BindGroupCreateInfo) -> BindGroup {
+        let n = create_info.entries.len();
 
+        // check if binding_points are out of range or duplicate
+        let mut flag = vec![false; n];
+        for i in 0..n {
+            let binding_point = create_info.entries[i].binding_point;
+
+            if binding_point >= n {
+                error!("bad bind group create info: binding point value out of range!");
+            } else if flag[binding_point] {
+                error!("bad bind group create info: binding point value duplicates!");
+            }
+
+            flag[binding_point] = true;
+        }
+
+        let mut res = BindGroup {
+            buffers: vec![Arc::<Vec<f64>>::default(); n],
+        };
+
+        // put each entry to its right position
+        for i in 0..n {
+            res.buffers[create_info.entries[i].binding_point] =
+                create_info.entries[i].buffer.clone();
+        }
+
+        res
     }
 
     pub fn get_data_from_location(&self, location: usize) -> Arc<Vec<f64>> {
-        self.buffer.clone()
+        self.buffers[location].clone()
+    }
+}
+
+impl BindGroupCreateInfo {
+    pub fn new() -> BindGroupCreateInfo {
+        BindGroupCreateInfo {
+            entries: Vec::<BindGroupEntry>::new(),
+        }
     }
 }
